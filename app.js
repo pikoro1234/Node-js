dc8570bd929780354d526dd7c478b8861b588017
jorge <donjuan.jfc@gmail.com>
2020-12-29 22:29:49 +0100

    creacion de primera ruta a home

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const {app} = require("./conexion");
 
-//matar procesos pkill nodejs o pkill node 
+const express = require("express");
+
+//para archivos estaticos
+app.use(express.static(__dirname + "/public"));
+
+//rutas de redireccionamiento
+app.use('/', require('./routers/rutas'));
 
 //motor de plantillas EJS
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-//rutas
-app.get("/", (req, res) =>{
-    res.render("index", {titulo: "Home"});
-})
+
+//matar procesos pkill nodejs o pkill node 
--- routers/rutas.js
+++ routers/rutas.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get("/", (req, res) =>{
+    res.render("index", {titulo: "Home"});
+})
+
 module.exports = router;
