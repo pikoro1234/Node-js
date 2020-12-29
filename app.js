07466152503a082af9a0d18a0b2b884676a8a5be
jorge <donjuan.jfc@gmail.com>
2020-12-29 22:36:00 +0100

    rutas y app modularizada en ficheros distintos

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.use(express.static(__dirname + "/public"));
 //rutas de redireccionamiento
 app.use('/', require('./routers/rutas'));
 
+//renderisamos si hay algun error de pagina con un midelware
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404"
+    })
+})
+
 //motor de plantillas EJS
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
--- conexion.js
+++ conexion.js
@@ -2,15 +2,11 @@ const express = require("express");
 
 const disparador = express();
 
-const puerto = 3000;
+const puerto = process.env.PORT || 3000;
 
 //abrimos el puerto a la escucha de peticiones
 disparador.listen(puerto, () =>{
     console.log("el servidor se ejecuta en el puerto "+puerto);
 });
 
-module.exports = {
-
-    app: disparador
-
-};
+module.exports = { app: disparador };
