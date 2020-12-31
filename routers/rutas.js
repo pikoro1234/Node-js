f391489beb5a32e3e93e4ad186cc7b00b6884e0f
jorge <donjuan.jfc@gmail.com>
2020-12-31 16:29:41 +0100

    insert de nuevo registro de usuario y redireccionamiento a index

--- routers/rutas.js
+++ routers/rutas.js
@@ -6,7 +6,7 @@ const conexionDB = require('../rmodels/conexionMongoose');
 
 const Usuario = require('../rmodels/usuario');
 
-router.get("/", (req, res) =>{
+router.get("/usuarios", (req, res) =>{
 
     try {
 
@@ -27,5 +27,35 @@ router.get("/registrate", (req, res) =>{
     
     res.render("registrate", {titulo: "Registrate"})
 })
+
+//ruta para el envio del  formulario
+router.post('/usuarios', async (req, res) =>{
+
+    const body = req.body
+
+    console.log(body);
+
+    try {
+
+        const usuarioDB = new Usuario(body)
+
+        await usuarioDB.save();
+
+        res.redirect('/usuarios')
+        
+    } catch (error) {
+        
+        console.log(error);
+    }
+})
+
+
+/* try {
+        const mascotaDB = new Mascota(body)
+        await mascotaDB.save()
+        res.redirect('/mascotas')
+    } catch (error) {
+        console.log('error', error)
+    } */
 
 module.exports = router;
